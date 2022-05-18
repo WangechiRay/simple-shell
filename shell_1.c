cb795333261d9805795dc85411818d832e844eab
wangechi <rahabsheshi22@gmail.com>
2022-05-18 23:39:42 +0000

    shell

--- shell_1.c
+++ shell_1.c
@@ -10,10 +10,28 @@
 
 int main(void)
 {
+	char *input;
 	char *buff;
 	size_t size = 10;
+	char *argv[2];
+
+	argv[1] = NULL;
+
+	while(1)
+	{
+		input = prompt();
+		if (input == NULL)
+		{
+			write(STDOUT_FILENO, "\n", 2);
+			exit(0);
+		}
+
+		argv[0] = input;
 
 	buff = malloc(sizeof(char) * size);
+
+	if(execve(argv[0], argv, NULL) == -1)
+		perror("error!");
 
 	printf("#cisfun$ ");
 	getline(&buff, &size, stdin);
