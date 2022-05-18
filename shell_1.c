86cb3d9c545a52263faa036a2ffea9b1939c0642
wangechi <rahabsheshi22@gmail.com>
2022-05-18 20:04:29 +0000

    shell

---
+++ shell_1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+
+/**
+ * main- writes UNIX command line interpreter.
+ *
+ * Return:0
+ */
+
+int main(void)
+{
+
+        char *buff;
+        size_t size = 10;
+
+        buff = malloc(sizeof(char) * size);
+
+        printf("#cisfun$ ");
+        getline(&buff, &size,stdin);
+
+        printf("%s\n", buff);
+        free(buff);
+
+        return (0);
+}
+
